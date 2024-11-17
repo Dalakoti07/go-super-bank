95f4a94f25627ef3f03c0a1ad45c0958d47c3114
dalakoti07 <saurabhdalakoti07@gmail.com>
2024-11-17 20:11:02 +0530

    added passwords

---
+++ api/user.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"net/http"
+	db "simplebank/db/sqlc"
+	"simplebank/utils"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"currency" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.Queries.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
